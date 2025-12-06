1c36347836be216fc96d6592bc7dcfbc5106c2ab
Vitor <vitorlorencone@gmail.com>
2025-12-06 06:24:40 -0300

    Final

--- src/main.cpp
+++ src/main.cpp
@@ -124,7 +124,7 @@ int main(int argc, char** argv) {
             world.rotateSpeedY = y;
         
         }else {
-            std::cerr << "Linha ignorada: " << line << "\n";
+            // Ignorar
         }
     }
 
